528115881fdde50831c77c471e192ce7cec1b2c1
REGG1N <151975374+REGG1N@users.noreply.github.com>
2024-04-11 04:05:02 +0100

    asd

--- server.js
+++ server.js
@@ -18,21 +18,14 @@ function sprawdzKlucz(req, res, next) {
 app.get('/ytdl', sprawdzKlucz, async (req, res) => {
     try {
         const key = req.query.key; // Extract key first
-        let url = req.query.url; // Then extract URL
-        console.log('Requested URL:', url); // Log URL for debugging
-
-        // Append workaround to bypass "I understand and wish to proceed" button
-        url += '&bpctr=9999999999&has_verified=1';
-
+        const url = req.query.url; // Then extract URL
         if (!url || !ytdl.validateURL(url)) {
             return res.status(400).send('Nieprawidłowy link YouTube, tytuł piosenki nie może zawierać emoji');
         }
         const info = await ytdl.getInfo(url);
-        if (!info) {
-            return res.status(404).send('Wskazany film na YouTube nie istnieje lub został usunięty.');
-        }
         const audioFormat = ytdl.chooseFormat(info.formats, { filter: 'audioonly' });
         res.header('Content-Disposition', `attachment; filename="${sanitizeFilename(info.videoDetails.title)}.mp3"`);
+
         res.header('Content-Type', 'audio/mpeg');
         const mp3Stream = ytdl(url, {
             format: 'mp3',
@@ -42,14 +35,9 @@ app.get('/ytdl', sprawdzKlucz, async (req, res) => {
         mp3Stream.pipe(res);
     } catch (error) {
         console.error('Error:', error);
-        if (error.message.includes('Video unavailable') || error.message.includes('This video is unavailable')) {
-            res.status(404).send('Wskazany film na YouTube nie istnieje lub został usunięty.');
-        } else {
-            res.status(500).send('Coś poszło nie tak, tytuł piosenki nie może zawierać emoji');
-        }
+        res.status(500).send('Coś poszło nie tak, tytuł piosenki nie może zawierać emoji');
     }
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
@@ -57,5 +45,5 @@ app.listen(PORT, () => {
 });
 
 function sanitizeFilename(filename) {
-    return filename.replace(/[/\\?%*:|"<>]/g, '-');
+    return filename.replace(/[^\w\s\-]/g, '-');
 }
